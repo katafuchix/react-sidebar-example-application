e3c09c92d35bbf4410faa13f5bed453e71ee40df
katafuchix <katafuchix@gmail.com>
2021-07-04 19:05:44 +0900

    mdb-react-ui-kit

--- src/pages/List.js
+++ src/pages/List.js
@@ -4,7 +4,10 @@ import classNames from "classnames";
 import { Container, Button } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
 import NavBar from "../components/content/Navbar";
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from "mdbreact";
+//import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBRow, MDBCol } from "mdbreact";
+
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn, MDBRipple } from 'mdb-react-ui-kit';
+
 
 class List extends React.Component {
 
@@ -15,7 +18,6 @@ class List extends React.Component {
         className={classNames("content", { "is-open": this.props.isOpen })}
       >
         <NavBar toggle={this.props.toggle} />
-
 
 
         <MDBRow>
